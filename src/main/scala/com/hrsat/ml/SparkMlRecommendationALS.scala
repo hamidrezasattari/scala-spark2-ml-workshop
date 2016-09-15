0783283b1d825ac477a99fb4c0339b7eb2fe93d9
hamid <hrsat110@hotmail.com>
2016-09-15 13:04:52 -0400

    prediction  by   spark ml recommendation ALS

--- src/main/scala/com/hrsat/ml/SparkMlRecommendationALS.scala
+++ src/main/scala/com/hrsat/ml/SparkMlRecommendationALS.scala
@@ -1,8 +1,10 @@
+package com.hrsat.ml
+
 import org.apache.log4j._
-import org.apache.spark.ml.recommendation.ALS.Rating
-import org.apache.spark.sql.types.{StructType, DataTypes, StructField}
 import org.apache.spark.ml.recommendation.ALS
+import org.apache.spark.ml.recommendation.ALS.Rating
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{DataTypes, StructField, StructType}
 
 //recommendation.ALS spark 2.0 for prediction task
 // data comes from ml-100k.zip http://grouplens.org/datasets/movielens/
@@ -40,6 +42,8 @@ object SparkMlRecommendationALS {
       .map(x => Rating(x.getAs[Int](0), x.getAs[Int](1), x.getAs[Float](2)))
       .toDF(colNames: _*).cache()
     ratings.printSchema()
+    // 3 added data for user 0
+    ratings.take(3).foreach(println)
 
     val als = new ALS()
       .setMaxIter(10)
@@ -49,8 +53,8 @@ object SparkMlRecommendationALS {
       .setRatingCol("rating")
     val model = als.fit(ratings)
 
-    // create 3 new record and assume  user not rated   them ( movies : 172, 133), and
-    // for movie : 50 , prediction should be almost same as  the user  already rated.
+    // create 3 new record and assume  user not rated (with -1 values)   them (two user new movies : 172, 133), and
+    // for movie : 50 , prediction should be almost same as  the user  already rated (0,50,2).
     // result predictions shows how model predict base on Collaborative filtering
 
 
