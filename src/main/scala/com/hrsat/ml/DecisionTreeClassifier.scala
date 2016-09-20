06316c2db44b68d087d5e8ce3d4eb29c2518e6ee
hamid <hrsat110@hotmail.com>
2016-09-20 19:45:31 -0400

    Decission tree  classification for pen digit dataset

---
+++ src/main/scala/com/hrsat/ml/DecisionTreeClassifier.scala
@@ -0,0 +1,84 @@
+package com.hrsat.ml
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by hamid on 20/09/16.
+  *  Decision Tree   classification for pen digit data set: datasource:http://archive.ics.uci.edu/ml/
+  */
+object DecisionTreeClassifier {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession
+      .builder
+      .master("local[*]")
+      .appName("decision_tree_classification")
+      .getOrCreate()
+
+    val training = spark.read
+      .option("header", false).option("delimiter", " ")
+      .format("libsvm").load("data/pendigits")
+    val testing = spark.read
+      .option("header", false).option("delimiter", " ")
+      .format("libsvm").load("data/pendigits.t")
+    training.take(5).foreach(println)
+
+    val labelIndexer = new StringIndexer()
+      .setInputCol("label")
+      .setOutputCol("indexedLabel")
+      .fit(training)
+    // Automatically identify categorical features, and index them.
+    val featureIndexer = new VectorIndexer()
+      .setInputCol("features")
+      .setOutputCol("indexedFeatures")
+      .setMaxCategories(4) // features with > 4 distinct values are treated as continuous.
+      .fit(training)
+
+
+    // Train a DecisionTree model.
+    val dt = new DecisionTreeClassifier()
+      .setLabelCol("indexedLabel")
+      .setFeaturesCol("indexedFeatures")
+
+    // Convert indexed labels back to original labels.
+    val labelConverter = new IndexToString()
+      .setInputCol("prediction")
+      .setOutputCol("predictedLabel")
+      .setLabels(labelIndexer.labels)
+
+    // Chain indexers and tree in a Pipeline.
+    val pipeline = new Pipeline()
+      .setStages(Array(labelIndexer, featureIndexer, dt, labelConverter))
+
+    // Train model. This also runs the indexers.
+    val model = pipeline.fit(training)
+
+    // Make predictions.
+    val predictions = model.transform(testing)
+
+    // Select example rows to display.
+    predictions.select("predictedLabel", "label", "features").show(5)
+
+    // Select (prediction, true label) and compute test error.
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("indexedLabel")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+    println("Test Error = " + (1.0 - accuracy))
+
+    val treeModel = model.stages(2).asInstanceOf[DecisionTreeClassificationModel]
+    println("Learned classification tree model:\n" + treeModel.toDebugString)
+
+
+
+
+  }
+}
